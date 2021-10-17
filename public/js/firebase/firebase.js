a8c2cf22f8041c398adf76e520ebf6e06dfea6f9
Kartik Derasari <derasarikartik@gmail.com>
2021-10-17 21:03:20 +0530

    Add Firebase Integration

---
+++ public/js/firebase/firebase.js
@@ -0,0 +1,15 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.1.1/firebase-app.js";
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "### FIREBASE API KEY ###",
+  authDomain: "### FIREBASE AUTH DOMAIN ###",
+  projectId: "### FIREBASE PROJECT ID ###",
+  storageBucket: "### FIREBASE STORAGE BUCKET ###",
+  messagingSenderId: "10728XXXXXXXXXX",
+  appId: "1:10728XXXXXXXXXX:web:c22f0b8XXXXXXXXXXXXXX",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
