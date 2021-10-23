470033656bb32cc960274f26880020b96358a6ad
DEEPAK KUMAR <78249949+DK85690@users.noreply.github.com>
2021-10-23 16:00:27 +0530

    Update firebase.js

--- public/js/firebase/firebase.js
+++ public/js/firebase/firebase.js
@@ -13,3 +13,25 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyAKUzvScYgYk4bAzwJNmKGUli14Shuj_JE",
+  authDomain: "psychic-glider-323209.firebaseapp.com",
+  projectId: "psychic-glider-323209",
+  storageBucket: "psychic-glider-323209.appspot.com",
+  messagingSenderId: "831157534955",
+  appId: "1:831157534955:web:06cfbddde36ca27ba13eae",
+  measurementId: "G-9716K52DDW"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
